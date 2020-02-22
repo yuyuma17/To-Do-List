c4e579a72f3a32e15db8e7f35bd9477176ac31da
Lacie <dark830917@gmail.com>
2020-02-22 19:59:11 +0800

    final

--- To-Do-List/Controller/TodoListViewController.swift
+++ To-Do-List/Controller/TodoListViewController.swift
@@ -11,7 +11,7 @@ import RealmSwift
 
 class TodoListViewController: UIViewController {
 
-    let realmManager = RealmManager.shared
+    fileprivate let realmManager = RealmManager.shared
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -19,33 +19,118 @@ class TodoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = editButtonItem
+        searchBar.autocapitalizationType = .none
         realmManager.loadData(reload: tableView)
     }
     
-    @IBAction func addNewTask(_ sender: UIBarButtonItem) {
-        
-        showAlert("test", "OK", nil)
-    }
-}
-
-extension TodoListViewController {
-    
-    func showAlert(_ alertTitle: String, _ confirmActionTitle: String, _ handler: ((UIAlertAction) -> Void)?) {
-        
-        let alert = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
-        
-        let confirmAction = UIAlertAction(title: confirmActionTitle, style: .default, handler: handler)
-        let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+    @IBAction func addNewTaskButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Task", message: nil, preferredStyle: .alert)
         
         var textField = UITextField()
         
-        alert.addTextField { (UITextField) in
-            UITextField.placeholder = "Write Something here..."
-            textField = UITextField
+        alert.addTextField { (field) in
+            field.placeholder = "Write Something..."
+            textField = field
         }
         
-        alert.addAction(confirmAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        let confirmAction = UIAlertAction(title: "OK", style: .default) { [weak self] (_) in
+            guard let self = self else { return }
+            let data = Data()
+            data.task = textField.text!
+            self.realmManager.saveData(data, reload: self.tableView)
+        }
         alert.addAction(cancelAction)
+        alert.addAction(confirmAction)
         present(alert, animated: true, completion: nil)
+    }
+}
+
+extension TodoListViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return realmManager.datas?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
+        
+        if let data = realmManager.datas?[indexPath.row] {
+            cell.textLabel?.text = data.task
+            cell.accessoryType = data.done ? .checkmark : .none
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let data = realmManager.datas?[indexPath.row] {
+            realmManager.changeDataStatus(data, reload: tableView)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        if tableView.isEditing == false {
+            tableView.isEditing = true
+            navigationItem.leftBarButtonItem?.title = "Done"
+        } else {
+            tableView.isEditing = false
+            navigationItem.leftBarButtonItem?.title = "Edit"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, _) in
+            guard let self = self else { return }
+            
+            if let data = self.realmManager.datas?[indexPath.row] {
+                self.realmManager.deleteData(data, at: indexPath, in: tableView)
+            }
+        }
+        let edit = UIContextualAction(style: .normal, title: "Revise") { [weak self] (_, _, _) in
+            guard let self = self else { return }
+            
+            let alert = UIAlertController(title: "Edit Task", message: nil, preferredStyle: .alert)
+            
+            var textField = UITextField()
+            
+            alert.addTextField { (field) in
+                field.placeholder = "Write Something..."
+                textField = field
+                textField.text = self.realmManager.datas?[indexPath.row].task
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            let confirmAction = UIAlertAction(title: "OK", style: .default) { [weak self] (_) in
+                guard let self = self else { return }
+                
+                if let data = self.realmManager.datas?[indexPath.row] {
+                    self.realmManager.reviseData(data, task: textField.text!, reload: tableView)
+                }
+            }
+            alert.addAction(cancelAction)
+            alert.addAction(confirmAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+        edit.backgroundColor = UIColor.systemGreen
+        return UISwipeActionsConfiguration(actions: [delete, edit])
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+        if searchBar.text?.count == 0 {
+            realmManager.loadData(reload: tableView)
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        } else {
+            realmManager.datas = realmManager.datas?.filter("task CONTAINS[cd] %@", searchBar.text!)
+            tableView.reloadData()
+        }
     }
 }
